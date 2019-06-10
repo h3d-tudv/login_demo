5ab8f9458ae0135cb716f7af667be59699afacea
dotu <tudv@house3d.net>
2019-06-10 23:48:15 +0700

    Login cơ bản, có validate dữ liệu

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -8,6 +9,13 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  bool _showPass = false;
+  TextEditingController _userController = new TextEditingController();
+  TextEditingController _passController = new TextEditingController();
+  var _userNameErr = "Tài khoản không hợp lệ";
+  var _passErr = "Mật khẩu phải trên 6 kí tự";
+  var _userInvalid = false;
+  var _passInvalid = false;
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -22,47 +30,126 @@ class _MyAppState extends State<MyApp> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Padding(
-                padding: const EdgeInsets.fromLTRB( 0, 0, 0, 40),
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
                 child: Container(
-                  width: 70,
+                    width: 70,
                     height: 70,
                     padding: EdgeInsets.all(15),
                     decoration: BoxDecoration(
                       shape: BoxShape.circle,
                       color: Colors.black12,
                     ),
-                    child: FlutterLogo()
-                ),
+                    child: FlutterLogo()),
               ),
               Padding(
                 padding: EdgeInsets.fromLTRB(0, 0, 0, 40),
-                child: Text("Hello\nWelcome back", style: TextStyle(
-                    fontWeight: FontWeight.bold, color: Colors.black, fontSize: 30
-                )
-              ),),
+                child: Text("Hello\nWelcome back",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        fontSize: 30)),
+              ),
               Padding(
                 padding: EdgeInsets.fromLTRB(0, 0, 0, 40),
                 child: TextField(
+                  controller: _userController,
                   style: TextStyle(fontSize: 20, color: Colors.black),
-                  decoration: InputDecoration(labelText: "UserName", labelStyle: TextStyle(color: Colors.black12, fontSize: 18, )),
+                  decoration: InputDecoration(
+                      labelText: "UserName",
+                      errorText: _userInvalid ? _userNameErr : null,
+                      labelStyle: TextStyle(
+                        color: Colors.black12,
+                        fontSize: 18,
+                      )),
                 ),
               ),
-              Stack(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(0, 0, 0, 40),
-                      child: TextField(
-                      obscureText: true,
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 40),
+                child: Stack(
+                  alignment: AlignmentDirectional.centerEnd,
+                  children: <Widget>[
+                    TextField(
+                      controller: _passController,
+                      obscureText: !_showPass,
                       style: TextStyle(fontSize: 20, color: Colors.black),
-                      decoration: InputDecoration(labelText: "Password", labelStyle: TextStyle(color: Colors.black12, fontSize: 18, )),
+                      decoration: InputDecoration(
+                          labelText: "Password",
+                          errorText: _passInvalid ? _passErr : null,
+                          labelStyle: TextStyle(
+                            color: Colors.black12,
+                            fontSize: 18,
+                          )),
                     ),
-                  ),
-                ],
+                    GestureDetector(
+                      onTap: onToggleShowPass,
+                      child: Text(
+                        _showPass ? "HIDE" : "SHOW",
+                        style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 13,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
               ),
+              SizedBox(
+                width: double.infinity,
+                height: 56,
+                child: RaisedButton(
+                  color: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8))),
+                  onPressed: onSignInClicked,
+                  child: Text(
+                    "SIGN IN",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                child: Container(
+                    height: 100,
+                    width: double.infinity,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text("NEW USER? SIGN UP",
+                            style:
+                                TextStyle(color: Colors.black12, fontSize: 15)),
+                        Text("FORGOT PASSWORD?",
+                            style: TextStyle(color: Colors.blue, fontSize: 15)),
+                      ],
+                    )),
+              )
             ],
           ),
         ),
       ),
     );
+  }
+
+  void onSignInClicked() {
+    setState(() {
+      if (_userController.text.length < 6 || !_userController.text.contains("@"))
+        _userInvalid = true;
+      else
+        _userInvalid = false;
+
+      if (_passController.text.length < 6)
+        _passInvalid = true;
+      else
+        _passInvalid = false;
+    });
+  }
+
+  void onToggleShowPass() {
+    setState(() {
+      _showPass = !_showPass;
+    });
   }
 }
